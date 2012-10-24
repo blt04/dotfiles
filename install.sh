2c7dd71d2851e188d9b70c9e3011f0cbbcab4ec3
Brandon Turner <bt@brandonturner.net>
2012-10-24 00:34:53 -0500

    Allow overwriting all files with a "-y" argument

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ IGNORE=( $(basename $0) . .. .git .gitignore README.markdown git-completion.bash
 
 HOMEDIR=`cd ~; pwd`
 
+# Poor man's option processing
+if [ "$1" == "-y" ]; then
+  OVERWRITE="yes"
+elif [ "$1" == "-n" ]; then
+  OVERWRITE="no"
+fi
+
 cd `dirname $0`
 
 for f in `ls`; do
@@ -23,13 +30,17 @@ for f in `ls`; do
     fi
 
     if [[ -e "$HOMEDIR/.$f" ]]; then
-        #echo -n "File exists: $HOMEDIR/.$f  Replace? [y/N] "
-        read -p "File exits: $HOMEDIR/.$f  Replace? [y/N] " -n 1 ans
-        if [[ ${ans} != "" ]]; then
-            echo
-        fi
-        if [[ ${ans} != 'y' && ${ans} != 'Y' ]]; then
+        if [ "$OVERWRITE" == "no" ]; then
             continue;
+        elif ! [ "$OVERWRITE" == "yes" ]; then
+            #echo -n "File exists: $HOMEDIR/.$f  Replace? [y/N] "
+            read -p "File exits: $HOMEDIR/.$f  Replace? [y/N] " -n 1 ans
+            if [[ ${ans} != "" ]]; then
+                echo
+            fi
+            if [[ ${ans} != 'y' && ${ans} != 'Y' ]]; then
+                continue;
+            fi
         fi
         rm -rf "$HOMEDIR/.$f"
     fi
